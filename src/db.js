3a6f37c321976f98a27e29caac3ebfe3471d0696
edgardo90 <edgardo9000@gmail.com>
2022-04-01 06:06:12 -0300

    ya no se que hacer

--- src/db.js
+++ src/db.js
@@ -9,7 +9,7 @@ const {
 const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  // dialect: "postgres",
+  dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
